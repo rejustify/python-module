572279da1e64f8e4d3c043269661797f1554ec32
7 <cloud@rejustify.com>
2020-08-06 17:17:05 +0200

    Typo correction with License classifier

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: GPL-3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
